59be69640f3f4d1c9d22a3b55fc671c1f4f11e89
Sheldon38 <109095852+Sheldon38@users.noreply.github.com>
2024-02-02 19:16:56 +0530

    Delete imgread.m

--- imgread.m
+++
@@ -1,7 +0,0 @@
-%showing image in matlab
-img = imread('E:\clg work\Sem 4\Matlab Files\ABC.jpg');
-fid = fopen('E:\clg work\Sem 4\Matlab Files\imtotxt.txt', 'w');
-if fid == -1, error('Cannot open file'); end
-%fprintf(fid, '%d\n%d\n', size(img));  % Assuming it is an RGB image
-fprintf(fid, '%x\n', img(:));
-fclose(fid);
