4b8b719424c3f883c00299a78b7f5dc33679b969
zen0-5338 <shahjeet767@gmail.com>
2024-02-09 19:49:37 +0530

    use chaos generator in sbox tb

--- tb/substitution_box_generator_tb.v
+++ tb/substitution_box_generator_tb.v
@@ -1,15 +1,30 @@
+`include "../RTL_verilog/chaos_generator.v"
 `include "../RTL_verilog/substitution_box_generator.v"
+
+`define CLOCK_PERIOD 10
 
 module substitution_box_generator_tb;
 
 reg clk;
 reg reset = 'b1;
 reg enable_bar = 'b0;
-reg [31:0] chaotic_signal_x1, chaotic_signal_x2, chaotic_signal_x3;
+wire [31:0] chaotic_signal_x1, chaotic_signal_x2, chaotic_signal_x3;
+reg [31:0] x1_initial, x2_initial, x3_initial;
 wire ready;
 wire [7:0] sbox[16][16];
 
 shortreal v1, v2, v3;
+
+chaos_generator chaos_generator_object(
+    clk,
+    reset,
+    x1_initial,
+    x2_initial,
+    x3_initial,
+    chaotic_signal_x1,
+    chaotic_signal_x2,
+    chaotic_signal_x3
+);
 
 substitution_box_generator DUT(
     sbox,
@@ -22,24 +37,24 @@ substitution_box_generator DUT(
     clk
 );
 
-always #5 clk=~clk;
+always #(`CLOCK_PERIOD/2) clk=~clk;
 
 initial	begin
 	clk = 1'b0;
+    enable_bar = 1'b0;
+    reset = 1'b1;
 
-	while(v1<0.5) begin
-		v1 = v1 + 0.01;
-		chaotic_signal_x1 = $shortrealtobits(v1);
-		#10;
-	end
-
+    #`CLOCK_PERIOD reset = 'b0;
+    forever begin
+        #`CLOCK_PERIOD;
+    end
 end
 
 reg [7:0] mem[16][16];
 initial	begin
-	if(substitution_box_ready)) begin
+	if(substitution_box_ready) begin
 		mem = sbox;
-        $writememh("substitution_box.txt",mem);
+        $writememh("./output/substitution_box.txt",mem);
         $finish();
 	end
 end
