d98d17468cd7f36b258a9b3a0238e91498726726
zen0-5338 <shahjeet767@gmail.com>
2024-02-09 19:48:51 +0530

    fixed syntax errors

--- RTL/substitution_box_generator.py
+++ RTL/substitution_box_generator.py
@@ -3,10 +3,10 @@ print(
 `include "../RTL/multiplier/Multiplication.v"
 
 module substitution_box_generator
-(
+#(
     parameter TABLE_ROWS = 16,
     parameter KEY_SIZE = 128, // TODO: implement variable key size
-    parameter USING_IEEE_754_DP = 0, // TODO: use sp or dp format based on this param 
+    parameter USING_IEEE_754_DP = 0// TODO: use sp or dp format based on this param 
 )
 (
     output reg [7:0] substitution_box[0:TABLE_ROWS-1][0:(KEY_SIZE >> 3)-1],
@@ -16,13 +16,13 @@ module substitution_box_generator
     input [31:0] chaotic_signal_x3,
     input reset,
     input enable_bar,
-    input clk,
+    input clk
 );
     
     localparam power_ten6_ieee754_sp = 32'h4974_2400;
     
     // Map to track stored s-box values
-    reg sbox_value_map[256];
+    reg sbox_value_map[0:255];
     // Counter register to track number of stored values. TODO: implement parameterised counter size 
     reg [7:0] sbox_counter = 'b0;
     
@@ -31,7 +31,7 @@ module substitution_box_generator
     // trash/temp vars
     wire [22:0] x1_decimal_shifted_mantissa,
         x2_decimal_shifted_mantissa,
-        x3_decimal_shifted_mantissa,
+        x3_decimal_shifted_mantissa;
     // sbox vector generation bytes
     wire [7:0] m1, m2, m3, v;
     
@@ -42,34 +42,34 @@ module substitution_box_generator
     Multiplication x2_decimal_digit_shifter(.a_operand(chaotic_signal_x2),.b_operand(power_ten6_ieee_754), .result(x2_decimal_shifted));
     Multiplication x3_decimal_digit_shifter(.a_operand(chaotic_signal_x3),.b_operand(power_ten6_ieee_754), .result(x3_decimal_shifted));
     
-    
     // Obtain the part of mantissa having the digits left of decimal point by shifting exponent amount
-    assign {m1,x1_mantissa_shifted} = {8'h01, x1_decimal_shifted[22:0]} << ((x1_decimal_shifted[30-:8] <= 127) ? 'd0 : x1_decimal_shifted[30-:8] - 'd127)
-    assign {m2,x2_mantissa_shifted} = {8'h01, x2_decimal_shifted[22:0]} << ((x2_decimal_shifted[30-:8] <= 127) ? 'd0 : x2_decimal_shifted[30-:8] - 'd127)
-    assign {m3,x3_mantissa_shifted} = {8'h01, x3_decimal_shifted[22:0]} << ((x3_decimal_shifted[30-:8] <= 127) ? 'd0 : x3_decimal_shifted[30-:8] - 'd127)
+    assign {m1,x1_mantissa_shifted} = {8'h01, x1_decimal_shifted[22:0]} << ((x1_decimal_shifted[30-:8] <= 127) ? 'd0 : x1_decimal_shifted[30-:8] - 'd127);
+    assign {m2,x2_mantissa_shifted} = {8'h01, x2_decimal_shifted[22:0]} << ((x2_decimal_shifted[30-:8] <= 127) ? 'd0 : x2_decimal_shifted[30-:8] - 'd127);
+    assign {m3,x3_mantissa_shifted} = {8'h01, x3_decimal_shifted[22:0]} << ((x3_decimal_shifted[30-:8] <= 127) ? 'd0 : x3_decimal_shifted[30-:8] - 'd127);
     
     // generate vector to be stored in sbox 
     assign v = (m1 ^ m2 ^ m3) >> 1;
     
-    always @(posedge(clk) and !enable_bar) begin
-        if (reset) begin
-            substitution_box_ready <= 1'b0;
-            sbox_counter <= 'b0;
-            for(integer i = 0; i < TABLE_ROWS; i++) begin
-                substitution_box[i] <= 'b0;
+    always @(posedge(clk)) begin
+        if (!enable_bar) begin
+            if (reset) begin
+                substitution_box_ready <= 1'b0;
+                sbox_counter <= 'b0;
+                for(integer i = 0; i < TABLE_ROWS; i++) begin
+                    substitution_box[i] <= 'b0;
+                end
+                for(integer i = 0; i < 256; i++) begin
+                    sbox_value_map[i] <= 1'b0;
+                end
             end
-            for(integer i = 0; i < 256; i++) begin
-                sbox_value_map[i] <= 1'b0;
+            else if (!sbox_value_map[v]) begin
+                substitution_box[j >> 4][j[3:0]] <= v:
+                sbox_value_map[v] <= 1'b1;
+                {substitution_box_ready, j} <= j + 1;
             end
-        end
-        else if (!sbox_value_map[v]) begin
-            substitution_box[j >> 4][j[3:0]] <= v:
-            sbox_value_map[v] <= 1'b1;
-            {substitution_box_ready, j} <= j + 1;
         end
     end
 
 endmodule;
-
 """
 )
