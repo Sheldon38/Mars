9e525659e2654124a49640f57881d95a40231fc5
zen0-5338 <shahjeet767@gmail.com>
2024-02-10 18:30:47 +0530

    final tb for sbox generator

--- tb/substitution_box_generator_tb.v
+++ tb/substitution_box_generator_tb.v
@@ -11,9 +11,7 @@ reg enable_bar = 'b0;
 wire [31:0] chaotic_signal_x1, chaotic_signal_x2, chaotic_signal_x3;
 reg [31:0] x1_initial, x2_initial, x3_initial;
 wire ready;
-wire [7:0] sbox[16][16];
-
-shortreal v1, v2, v3;
+wire [7:0] sbox[0:15][0:15];
 
 chaos_generator chaos_generator_object(
     clk,
@@ -37,26 +35,27 @@ substitution_box_generator DUT(
     clk
 );
 
+reg [7:0] mem[0:15][0:15];
+
 always #(`CLOCK_PERIOD/2) clk=~clk;
 
 initial	begin
 	clk = 1'b0;
     enable_bar = 1'b0;
     reset = 1'b1;
-
+    x1_initial = 32'h3DCC_CD53; // 0.1
+	x2_initial = 32'h3C23_D70A; // 0.01
+	x3_initial = 32'h0;
     #`CLOCK_PERIOD reset = 'b0;
-    forever begin
-        #`CLOCK_PERIOD;
+    
+    wait(ready == 1);
+    for (integer i = 0; i < 16; i++) begin
+        for (integer j = 0; j < 16; j++) begin
+            mem[i][j] = sbox[i][j];
+        end
     end
-end
-
-reg [7:0] mem[16][16];
-initial	begin
-	if(substitution_box_ready) begin
-		mem = sbox;
-        $writememh("./output/substitution_box.txt",mem);
-        $finish();
-	end
+    $writememh("./output/substitution_box.txt", mem);
+    $finish();
 end
 
 endmodule
